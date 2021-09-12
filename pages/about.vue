22631414300085d74f32a3cd296ca3ae1c9e4203
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2021-09-12 15:00:35 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="About Us"
-      subtitle="Example About Page"
+      title="About Jules"
+      subtitle="The Vale of Belvoir dog walker"
       image="/uploads/frostyfield.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
-          <p>
-            <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
-            </strong>
-          </p>
+          <h3>Happy dogs</h3>
+       
           <p>
             Hello, I’m Jules
 </p>
@@ -24,49 +20,6 @@ I am an independent dog walker in the Vale Of Belvoir. I also offer day care, ov
 I have a dog of my own called Ernie. He is a friendly and lively red cocker spaniel who loves to socialise with other dogs and is a much loved member of the family!
           </p>
 
-         
-        </div>
-
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Cool Stuff We Do
-              </p>
-              <p class="subtitle">
-                Sooo Cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Other Cool Things
-              </p>
-              <p class="subtitle">
-                Just as cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                All the cool
-              </p>
-              <p class="subtitle">
-                that is all
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
         </div>
       </template>
 
