94ebd57d9314c6fcd25dd352fe2d7634eb3af2be
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2021-09-19 18:05:07 +0100

    Update SiteFooter.vue

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -3,8 +3,7 @@
    Dog walker based in Granby, Nottingham, walking dogs from villages across the Vale of Belvoir, offering day care and overnight stays for your dog.
 <br /> 
 <b>Email</b> julesdogwalks@gmail.com 
-<br /> 
-<i style="font-size:24px" class="fa">&#xf0e0;</i> 07393 746756 
+<br />&#xf0e0; 07393 746756 
   </footer>
 </template>
 
